b65551622f4adfab30707d9ac64b3dcd0d8fc879
rlatmddn956 <rlatmddn956@gmail.com>
2025-08-28 21:04:38 +0900

    v0.7 DataType & Casting

--- week03/src/DataType.java
+++ week03/src/DataType.java
@@ -1,5 +1,6 @@
 public class DataType {
     public static void main(String[] args) {
+        // Date Type and Casting
         int score = 87;
         float gpa = 4.13F;
         System.out.println(score);
