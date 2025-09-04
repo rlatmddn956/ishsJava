7b16c96b7dd310f475be0b36476a95bd674acfac
rlatmddn <rlatmddn956@gmail.com>
2025-09-04 21:10:52 +0900

    íGugudan2

--- week04/src/GugudanGame.java
+++ week04/src/GugudanGame.java
@@ -1,14 +1,15 @@
 import java.util.Scanner;
 
 public class GugudanGame {
-    static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("단을 입력하세요 : ");
+        System.out.print("단을 입력하세요 : ");
         int dan = scanner.nextInt();
-
-        for(int i=1; i<10; i++){
+        if(dan < 2 || dan > 9) {
+            System.out.println("단으 2~9 사이의 값입니다.");
+        }else{
+            for(int i=1; i<10; i++){
             System.out.println(dan + " x " + i + " = " + (dan*i));
         }
     }
-
 }
