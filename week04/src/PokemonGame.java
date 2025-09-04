d23b07e1428984a65b956a825cb4cacbee0eff58
rlatmddn <rlatmddn956@gmail.com>
2025-09-04 20:30:36 +0900

    1.2 If Else

--- week04/src/PokemonGame.java
+++ week04/src/PokemonGame.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 class Pokemon{
     // concrete class (can produce object)
     int hp;
@@ -6,10 +8,28 @@ class Pokemon{
 public class PokemonGame {
     public static void main(String[] args) {
         Pokemon pikachu = new Pokemon();
+        //Pokemon squirtle = new Pokemon();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("체력을 입력하세요 : ");
+        pikachu.hp = scanner.nextInt();
+  //      squirtle.hp = 110;
+        //    System.out.println(squirtle.hp);
+   //     System.out.println(pikachu.hp);
+
+
+        Pokemon pikachu = new Pokemon();
         Pokemon squirtle = new Pokemon();
         pikachu.hp = 100;
         squirtle.hp = 110;
         System.out.println(squirtle.hp);
         System.out.println(pikachu.hp);
+
+        if(pikachu.hp > 99){
+            System.out.println("피카츄의 체력("+ pikachu.hp +")이 여유~");
+        }else{
+            System.out.println("피카츄의 체력("+ pikachu.hp +")이 부족!");
+        }
+
     }
 }
