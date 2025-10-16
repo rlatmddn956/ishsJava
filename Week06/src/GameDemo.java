3d7737c06b9548ee54f18191ab9e1d0a9a6c2bef
rlatmddn <rlatmddn956@gmail.com>
2025-10-16 19:44:15 +0900

    v0.2 Pokemon game

--- Week06/src/GameDemo.java
+++ Week06/src/GameDemo.java
@@ -1,5 +1,8 @@
 public class GameDemo {
     public static void main(String[] args) {
+        Pikachu myPikachu = new Pikachu(100);
+        Pikachu yourPikachu = new Pikachu(100);
         System.out.println("Game Start~");
+        myPikachu.attack(yourPikachu);
     }
 }
