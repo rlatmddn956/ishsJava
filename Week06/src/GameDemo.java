ea7708a5355952432c0f37482f1fda64a7b0e448
rlatmddn <rlatmddn956@gmail.com>
2025-10-16 21:02:37 +0900

    v0.7 Pokemon game

--- Week06/src/GameDemo.java
+++ Week06/src/GameDemo.java
@@ -1,15 +1,46 @@
 public class GameDemo {
     public static void main(String[] args) {
-        Pikachu myPikachu = new Pikachu(100, 27);
-        Pikachu yourPikachu = new Pikachu(100, 28);
-        Squirtle enemySquirtle = new Squirtle(120, 21);
-        System.out.println("Game Start~");
-        myPikachu.attack(enemySquirtle);
+        Pikachu p1 = new Pikachu(100, 27);
+        Pikachu p2 = new Pikachu(100, 28);
+        Squirtle s1 = new Squirtle(120, 21);
 
-        for (int i=0;i<5;i++)
-            enemySquirtle.attack(myPikachu);
+        System.out.println("배틀 시작!");
+        System.out.println(p1);
+        System.out.println(s1);
+        System.out.println("=============");
 
-        System.out.println(enemySquirtle);
-        System.out.println(myPikachu);
+        int turn = 1;
+
+        while(!p1.isFainted() && !s1.isFainted()){
+            System.out.println("턴 " + turn + "시작");
+            p1.attack(p2);
+            if (s1.isFainted()){
+                System.out.println(s1.getName() + "이(가) 기절했습니다! " + p1.getName() + " 승리!");
+                break;
+            }
+            s1.attack(p1);
+            if (s1.isFainted()) {
+                System.out.println(s1.getName() + "이(가) 기절했습니다! " + p1.getName() + " 승리!");
+                break;
+            }
+            System.out.println("============");
+            turn++;
+        }
+        System.out.println("배틀 종료!");
+
+
+
+
+//        Pikachu myPikachu = new Pikachu(100, 27);
+//        Pikachu yourPikachu = new Pikachu(100, 28);
+//        Squirtle enemySquirtle = new Squirtle(120, 21);
+//        System.out.println("Game Start~");
+//        myPikachu.attack(enemySquirtle);
+//
+//        for (int i=0;i<5;i++)
+//            enemySquirtle.attack(myPikachu);
+//
+//        System.out.println(enemySquirtle);
+//        System.out.println(myPikachu);
     }
 }
