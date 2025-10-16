c186b9d0b520ec70e132dbc3dc10174dd1dd20a8
rlatmddn <rlatmddn956@gmail.com>
2025-10-16 20:09:25 +0900

    v0.3 Pokemon game

--- Week06/src/GameDemo.java
+++ Week06/src/GameDemo.java
@@ -2,7 +2,9 @@ public class GameDemo {
     public static void main(String[] args) {
         Pikachu myPikachu = new Pikachu(100);
         Pikachu yourPikachu = new Pikachu(100);
+        Squirtle enemySquirtle = new Squirtle(100);
         System.out.println("Game Start~");
-        myPikachu.attack(yourPikachu);
+        myPikachu.attack(enemySquirtle);
+        enemySquirtle(myPikachu);
     }
 }
--- Week06/src/Squirtle.java
+++ Week06/src/Squirtle.java
@@ -1,2 +1,11 @@
-public class Squirtle {
+public class Squirtle extends Pokemon {
+    public Squirtle(int hp){
+        super("꼬부기", hp);
+        System.out.println("꼬북꼬북!");
+    }
+
+    @Override
+    public void attack(Pokemon target) {
+        System.out.println(name + " -> " + target.getName() + "에게 '물대포' 사용");
+    }
 }
