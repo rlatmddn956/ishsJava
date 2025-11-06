b2ddcb71a2c07915255deeff70b1d2674ee86cf3
rlatmddn <rlatmddn956@gmail.com>
2025-11-06 19:42:46 +0900

    collection v0.2

--- week09/src/collections/Fruit.java
+++ week09/src/collections/Fruit.java
@@ -4,24 +4,27 @@ public class Fruit {
     private String name;
     private int price;
 
-    public Fruit(int price, String name) {
-        this.price = price;
+    public Fruit() {
+    }
+
+    public Fruit(String name, int price) {
         this.name = name;
+        this.price = price;
+    }
 
-        public String getName () {
-            return name;
-        }
+    public String getName() {
+        return name;
+    }
 
-        public void setName (String name){
-            this.name = name;
-        }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-        public int getPrice () {
-            return price;
-        }
+    public int getPrice() {
+        return price;
+    }
 
-        public void setPrice ( int price){
-            this.price = price;
-        }
+    public void setPrice(int price) {
+        this.price = price;
     }
 }
